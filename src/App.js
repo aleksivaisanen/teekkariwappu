568803236933538a213f359e3b493a1f72dfbd25
aleksivaisanen <aleksi.vaisanen@elisanet.fi>
2019-09-09 18:39:09 +0300

    deleted all default nonsense and created new background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Coming soon!</h1>
+      <div className="background" />
+      <div className="counterContainer">
+        <h1>Coming soon!</h1>
+      </div>
     </div>
   );
 }
