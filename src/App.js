20a9a8e0e1ebed51dad08888f441b3a2d6b4ba7d
aleksivaisanen <aleksi.vaisanen@elisanet.fi>
2019-09-09 19:12:12 +0300

    added counter and made the image smaller

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Countdown from 'react-countdown-now'
 
 function App() {
   return (
     <div className="App">
       <div className="background" />
       <div className="counterContainer">
-        <h1>Coming soon!</h1>
+        <h1>Turun Teekkariwappu!</h1>
+        <p className="subheading">Coming soon...</p>
+        <Countdown
+          date={new Date('April 16, 2020 12:00:00')}
+        />
       </div>
     </div>
   );
