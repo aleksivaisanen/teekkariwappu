6197ae0de416e054266689088a18e9e01d96a88b
aleksivaisanen <aleksi.vaisanen@elisanet.fi>
2019-11-09 15:42:15 +0200

    start of the teekkariwappu app based on Traversy Medias server template

---
+++ config/default.js
@@ -0,0 +1,8 @@
+// default configs
+module.exports = {
+    "test": "test",
+    "mongoURI": process.env.MONGODB_URI,
+    "jwtSecret": process.env.JWT_SECRET,
+    "admin_username": process.env.ADMIN_USERNAME,
+    "admin_password": process.env.ADMIN_PASSWORD
+};
