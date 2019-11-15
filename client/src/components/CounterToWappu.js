ca1111d5a1b287a70fe7763d10aeb498e22924fc
aleksivaisanen <aleksi.vaisanen@elisanet.fi>
2019-11-15 20:55:01 +0200

    fixed counter numbers on mobile

--- client/src/components/CounterToWappu.js
+++ client/src/components/CounterToWappu.js
@@ -8,41 +8,41 @@ import { Row, Col } from 'reactstrap';
 // Renderer callback with condition
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
-      // Render a completed state
-      return null
+        // Render a completed state
+        return null
     } else {
-      // Render a countdown
-      return (
-        <div >
-			<h3 className="text-center coming-soon">
-				Coming Soon
+        // Render a countdown
+        return (
+            <div >
+                <h3 className="text-center coming-soon">
+                    Coming Soon
 			</h3>
 
-			<Row className="justify-content-center align-items-center">
-				<Col sm={3} className="wsize1 mb-2 d-flex flex-column align-items-start">
-                    <span className="counter-number text-left">{days}</span>
-                    <span className="counter-text">Days</span>
-                </Col>
+                <Row className="justify-content-center align-items-center">
+                    <Col sm={3} className="mb-2 wappu-counter-item">
+                        <span className="counter-number text-left">{days}</span>
+                        <span className="counter-text">Days</span>
+                    </Col>
 
-                <Col sm={3} className="wsize1 mb-2 d-flex flex-column align-items-start">
-                    <span className="counter-number text-left">{("0" + hours).slice(-2)}</span>
-                    <span className="counter-text">Hours</span>
-                </Col>
+                    <Col sm={3} className="mb-2 wappu-counter-item">
+                        <span className="counter-number text-left">{("0" + hours).slice(-2)}</span>
+                        <span className="counter-text">Hours</span>
+                    </Col>
 
-                <Col sm={3} className="wsize1 mb-2 d-flex flex-column align-items-start">
-                    <span className="counter-number text-left">{("0" + minutes).slice(-2)}</span>
-                    <span className="counter-text">Minutes</span>
-                </Col>
+                    <Col sm={3} className="mb-2 wappu-counter-item">
+                        <span className="counter-number text-left">{("0" + minutes).slice(-2)}</span>
+                        <span className="counter-text">Minutes</span>
+                    </Col>
 
-                <Col sm={3} className="wsize1 mb-2 d-flex flex-column align-items-start">
-                    <span className="counter-number text-left">{("0" + seconds).slice(-2)}</span>
-                    <span className="counter-text">Seconds</span>
-                </Col>
-            </Row>  
-        </div>
+                    <Col sm={3} className="mb-2 wappu-counter-item">
+                        <span className="counter-number text-left">{("0" + seconds).slice(-2)}</span>
+                        <span className="counter-text">Seconds</span>
+                    </Col>
+                </Row>
+            </div>
         )
     }
-  };
+};
 
 class CounterToWappu extends Component {
     constructor(props) {
@@ -57,8 +57,8 @@ class CounterToWappu extends Component {
     }
 
     componentDidUpdate(prevProps) {
-        if(prevProps.events.length === 0 && this.props.events.length > 0) {
-            this.setState({firstEventDate: new Date(this.props.events[0].date)})
+        if (prevProps.events.length === 0 && this.props.events.length > 0) {
+            this.setState({ firstEventDate: new Date(this.props.events[0].date) })
         }
     }
 
