b51b6d8a0e5c12dc6bd410b8133a95c24598123e
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2019-11-15 19:05:50 +0200

    added text links to footer and remowed skewing

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import SkewedContainer from 'sc-react';
 import digit from '../images/digit_boston.png';
 import nucleus from '../images/nucleus2.png';
 import fb from '../images/fb_logo.png';
@@ -10,24 +9,52 @@ import www from '../images/www_logo.png';
 class Footer extends Component {
     render() {
         return (
-            <SkewedContainer className="footer-skewed-container" top="left" bgColor="#f4943f">
-                <Container className="mt-5">
+            <Container fluid className="footer-container py-5">
+                <Container>
                     <Row>
                         <Col md={6} className="d-flex flex-row justify-content-center align-items-end">
                             <img className="guild-logo" src={digit} alt="Digit ry"/>
-                            <a href="https://fi-fi.facebook.com/digitry/"><img className="social-media" src={fb} alt="Digit ry Facebook" /></a>
-                            <a href="https://www.instagram.com/digitteekkari"><img className="social-media" src={ig} alt="Digit ry Instagram" /></a>
-                            <a href="https://digit.fi"><img className="social-media" src={www} alt="digit.fi" /></a>
+                            <div className="d-flex flex-column justify-content-left">
+                                <div className="d-flex flex-row justify-content-left align-items-center">
+                                    <a href="https://fi-fi.facebook.com/digitry/" className="social-media-link">
+                                        <img className="social-media" src={fb} alt="Digit ry Facebook" /> @digitry    
+                                    </a>
+                                </div>
+                                <div className="d-flex flex-row justify-content-left align-items-center">
+                                    <a href="https://www.instagram.com/digitteekkari" className="social-media-link">
+                                        <img className="social-media" src={ig} alt="Digit ry Instagram" /> @digitteekkari
+                                    </a>
+                                </div>
+                                <div className="d-flex flex-row justify-content-left align-items-center">
+                                    <a href="https://digit.fi" className="social-media-link">
+                                        <img className="social-media" src={www} alt="digit.fi" /> digit.fi
+                                    </a>
+                                </div>
+                            </div>
                         </Col>
                         <Col md={6} className="d-flex flex-row justify-content-center align-items-end">
                             <img className="guild-logo" src={nucleus} alt="Nucleus ry"/>
-                            <a href="https://fi-fi.facebook.com/Nucleusry/"><img className="social-media" src={fb} alt="Nucleus ry Facebook" /></a>
-                            <a href="https://www.instagram.com/nucleus_ry/?hl=fi"><img className="social-media" src={ig} alt="Nucleus ry Instagram" /></a>
-                            <a href="https://www.nucleus.fi"> <img className="social-media" src={www} alt="nucleus.fi" /></a>
+                            <div className="d-flex flex-column justify-content-left">
+                                <div className="d-flex flex-row justify-content-left align-items-center">
+                                    <a href="https://fi-fi.facebook.com/Nucleusry/" className="social-media-link">
+                                        <img className="social-media" src={fb} alt="Nucleus ry Facebook" /> @Nucleusry
+                                    </a>
+                                </div>
+                                <div className="d-flex flex-row justify-content-left align-items-center">
+                                    <a href="https://www.instagram.com/nucleus_ry/?hl=fi" className="social-media-link">
+                                        <img className="social-media" src={ig} alt="Nucleus ry Instagram" /> @nucleus_ry
+                                    </a>
+                                </div>
+                                <div className="d-flex flex-row justify-content-left align-items-center">
+                                    <a href="https://www.nucleus.fi" className="social-media-link"> 
+                                        <img className="social-media" src={www} alt="nucleus.fi" /> nucleus.fi
+                                    </a>
+                                </div>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
-            </SkewedContainer>
+            </Container>
         );
     }
 }
