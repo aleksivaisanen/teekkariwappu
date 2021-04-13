51e7093800373a60763f84bf829fc312e865c4f1
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2021-04-13 14:00:32 +0300

    Add ohjeet

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -3,122 +3,151 @@ import { connect } from 'react-redux';
 import { getEvents } from '../actions/eventActions';
 import { Container } from 'reactstrap';
 import EventListItem from './EventListItem';
+import { Row, Col } from 'reactstrap';
 
 class EventList extends Component {
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            collapse: null,
-        };
-    }
-
-    componentDidMount() {
-        this.props.getEvents();
-    }
-
-    // helper function for checking if the date is today
-    isToday(someDate) {
-        const today = new Date()
-        return someDate.getDate() === today.getDate() &&
-            someDate.getMonth() === today.getMonth() &&
-            someDate.getFullYear() === today.getFullYear()
-    }
-
-    // helper function for checking if the date is in the past (yesterday or older)
-    isPast(someDate) {
-        const today = new Date()
-        today.setHours(0, 0, 0, 0)
-        return someDate.getTime() < today.getTime()
-    }
-
-    // helper function for checking if the date is in the future (tomorrow or more)
-    isFuture(someDate) {
-        const today = new Date()
-        today.setHours(24, 0, 0, 0)
-        return someDate.getTime() >= today.getTime()
-    }
-
-    // helper function for travelling the DOM tree to find the correct parent
-    getClosest(elem, selector) {
-        for (; elem && elem !== document; elem = elem.parentNode) {
-            if (elem.matches(selector)) return elem;
-        }
-        return null;
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      collapse: null,
     };
+  }
 
-    toggle(e) {
-        let event;
-        if (e.target.className === 'card-header') {
-            event = e.target.dataset.event;
-        } else {
-            event = this.getClosest(e.target, '.card-header').dataset.event;
+  componentDidMount() {
+    this.props.getEvents();
+  }
+
+  // helper function for checking if the date is today
+  isToday(someDate) {
+    const today = new Date()
+    return someDate.getDate() === today.getDate() &&
+      someDate.getMonth() === today.getMonth() &&
+      someDate.getFullYear() === today.getFullYear()
+  }
+
+  // helper function for checking if the date is in the past (yesterday or older)
+  isPast(someDate) {
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+    return someDate.getTime() < today.getTime()
+  }
+
+  // helper function for checking if the date is in the future (tomorrow or more)
+  isFuture(someDate) {
+    const today = new Date()
+    today.setHours(24, 0, 0, 0)
+    return someDate.getTime() >= today.getTime()
+  }
+
+  // helper function for travelling the DOM tree to find the correct parent
+  getClosest(elem, selector) {
+    for (; elem && elem !== document; elem = elem.parentNode) {
+      if (elem.matches(selector)) return elem;
+    }
+    return null;
+  };
+
+  toggle(e) {
+    let event;
+    if (e.target.className === 'card-header') {
+      event = e.target.dataset.event;
+    } else {
+      event = this.getClosest(e.target, '.card-header').dataset.event;
+    }
+    this.setState({ collapse: this.state.collapse === String(event) ? null : String(event) });
+  }
+
+  render() {
+    const { collapse } = this.state;
+    const { events } = this.props;
+
+    events.sort((a, b) => new Date(a.date) - new Date(b.date))
+    const pastEvents = events.filter(event => { return this.isPast(new Date(event.date)) })
+    const currentEvents = events.filter(event => { return this.isToday(new Date(event.date)) })
+    const futureEvents = events.filter(event => { return this.isFuture(new Date(event.date)) })
+
+    return (
+      <Container className="my-5 pt-5 eventlist-container">
+        {window.location.href.indexOf('/admin') === -1 &&
+          <>
+            <h3 className="text-center my-3">Ohjeet</h3>
+            <Row>
+              <Col sm={12} className="ohje-container">
+                <p>
+                  Perinteiseen tapaan tästäkin Wapusta saa Wappuputkimerkkejä tapahtumiin osallistumalla. Seuraa itse osallistumistasi, ja kerro siitä Wapun jälkeen Digitin tai Nucleuksen hallituslaiselle saadaksesi merkit. Merkkien määrä riippuu osallistumistasi tapahtumissa seuraavasti:
+                </p>
+                <p>
+                  3 tapahtumaa 1 merkki <br />
+                  6 tapahtumaa 2 merkkiä <br />
+                  9 tapahtumaa 3 merkkiä <br />
+                  12 tapahtumaa 4 merkkiä <br />
+                </p>
+                <p>
+                  Tapahtumien osallistujalistoja seurataan, joten olethan rehellinen noutaessasi merkkejä.
+                </p>
+                <p>
+                  Wapun viettäjille tehdään oma Discord-kanava, jota käytämme eri tapahtumiin ja viestintään. Wappu-Discordia käytetään myös päiväkohtaisten haasteiden taltiointiin. Discordiin liittymisestä ilmoitetaan myöhemmin.
+                </p>
+                <p>
+                  Koko Wapun ajan on käynnissä Wappuseikkailu. Seikkailuun kuuluu erilaisia rasteja, joiden avulla pääsee kulkemaan ympäri Turkua ja ratkaisemaan kiperiä pulmia ja visaisia vinkkejä. Wappuseikkailua voi suorittaa täysin omaan tahtiin koko Wapun ajan yksin tai maksimissaan 4 hengen ryhmissä. Lisää infoa sekä tarkemmat ohjeet Wappuseikkailuun tulevat tälle sivulle myöhemmin.
+                </p>
+
+              </Col>
+            </Row>
+          </>
         }
-        this.setState({ collapse: this.state.collapse === String(event) ? null : String(event) });
-    }
-
-    render() {
-        const { collapse } = this.state;
-        const { events } = this.props;
-
-        events.sort((a, b) => new Date(a.date) - new Date(b.date))
-        const pastEvents = events.filter(event => { return this.isPast(new Date(event.date)) })
-        const currentEvents = events.filter(event => { return this.isToday(new Date(event.date)) })
-        const futureEvents = events.filter(event => { return this.isFuture(new Date(event.date)) })
-
-        return (
-            <Container className="my-5 pt-5 eventlist-container">
-                {events.length === 0 &&
-                    <h3 className="text-center my-3">Ei tapahtumia!</h3>
-                }
-                {currentEvents.length > 0 &&
-                    <h3 className="text-center my-3">Tapahtuma(t) tänään</h3>
-                }
-                {currentEvents.length > 0 &&
-                    currentEvents.map((event) =>
-                        <EventListItem
-                            key={event._id.toString()}
-                            event={event}
-                            collapse={collapse}
-                            toggle={this.toggle}
-                        />)
-                }
-                {futureEvents.length > 0 &&
-                    <h3 className="text-center my-3">Tulevat tapahtumat</h3>
-                }
-                {futureEvents.length > 0 &&
-                    futureEvents.map((event) =>
-                        <EventListItem
-                            key={event._id.toString()}
-                            event={event}
-                            collapse={collapse}
-                            toggle={this.toggle}
-                        />)
-                }
-                {pastEvents.length > 0 &&
-                    <h3 className="text-center my-3">Menneet tapahtumat</h3>
-                }
-                {pastEvents.length > 0 &&
-                    pastEvents.map((event) =>
-                        <EventListItem
-                            key={event._id.toString()}
-                            event={event}
-                            collapse={collapse}
-                            toggle={this.toggle}
-                        />)
-                }
-                <br />
-            </Container>
-        );
-    }
+        {events.length === 0 &&
+          <h3 className="text-center my-3">Ei tapahtumia!</h3>
+        }
+        {currentEvents.length > 0 &&
+          <h3 className="text-center my-3">Tapahtuma(t) tänään</h3>
+        }
+        {currentEvents.length > 0 &&
+          currentEvents.map((event) =>
+            <EventListItem
+              key={event._id.toString()}
+              event={event}
+              collapse={collapse}
+              toggle={this.toggle}
+            />)
+        }
+        {futureEvents.length > 0 &&
+          <h3 className="text-center my-3">Tulevat tapahtumat</h3>
+        }
+        {futureEvents.length > 0 &&
+          futureEvents.map((event) =>
+            <EventListItem
+              key={event._id.toString()}
+              event={event}
+              collapse={collapse}
+              toggle={this.toggle}
+            />)
+        }
+        {pastEvents.length > 0 &&
+          <h3 className="text-center my-3">Menneet tapahtumat</h3>
+        }
+        {pastEvents.length > 0 &&
+          pastEvents.map((event) =>
+            <EventListItem
+              key={event._id.toString()}
+              event={event}
+              collapse={collapse}
+              toggle={this.toggle}
+            />)
+        }
+        <br />
+      </Container>
+    );
+  }
 }
 
 
 const mapStateToProps = state => ({
-    events: state.event.events,
+  events: state.event.events,
 });
 
 export default connect(
-    mapStateToProps,
-    { getEvents }
+  mapStateToProps,
+  { getEvents }
 )(EventList);
