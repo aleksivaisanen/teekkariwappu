6855c4a3779a94f752dfd0fb198c2e0a95f15d2a
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2021-04-13 14:28:16 +0300

    i made everything that Matti made me do

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,33 +3,45 @@ import { Jumbotron, Container } from 'reactstrap';
 import CounterToWappu from './CounterToWappu';
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header-class">
-                <Jumbotron fluid>
-                    <Container>
-                        <div>
-                            <h1 className="heading text-center">TURUN TEEKKARIWAPPU</h1>
-                            <CounterToWappu />
-                            <p className="text-center smaller-font">
-                                <i>#turunwappu</i>
-                            </p>
-                            <p className="text-center medium-font">
-                                Wappufiilistä luomassa<br />
-                                <b><a href="https://digit.fi">Digit</a></b> & <b><a href="https://www.nucleus.fi">Nucleus</a></b><br />
-                                <span className="menossa-mukana">
-                                    <p>
-                                        Menossa mukana myös <br />
-                                        <b><a href="http://teekkarikomissio.utu.fi/fi/index.html">Teekkarikomissio</a></b> & <b><a href="https://www.asteriski.fi">Asteriski</a></b>
-                                    </p>
-                                </span>
-                            </p>
-                        </div>
-                    </Container>
-                </Jumbotron>
+  render() {
+    return (
+      <div className="header-class">
+        <Jumbotron fluid>
+          <Container>
+            <div>
+              <h1 className="heading text-center">TURUN TEEKKARIWAPPU</h1>
+              <CounterToWappu />
+              <p className="text-center smaller-font">
+                <i>#turunwappu</i>
+              </p>
+              <p className="text-center medium-font">
+                Wappufiilistä luomassa<br />
+                <b>
+                  <a href="https://digit.fi">Digit</a>
+                </b>,
+                                  <b>
+                  <a href="https://www.nucleus.fi"> Nucleus</a>
+                </b>,
+                                    <b>
+                  <a href="https://www.facebook.com/MateriaalitekniikankiltaAdamasry"> Adamas </a>
+                </b>&
+                                    <b>
+                  <a href="https://www.facebook.com/KonetekniikkaMachinary/"> Machina</a>
+                </b>
+                <br />
+                <span className="menossa-mukana">
+                  <p>
+                    Menossa mukana myös <br />
+                    <b><a href="http://teekkarikomissio.utu.fi/fi/index.html">Teekkarikomissio</a></b> & <b><a href="https://www.asteriski.fi">Asteriski</a></b>
+                  </p>
+                </span>
+              </p>
             </div>
-        );
-    }
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
 }
 
 export default Header;
